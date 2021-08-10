352eab71673d90ed00cd731f5b058222e4cad3dc
MasatoooNkazatooo <Saccersaccer04@icloud.com>
2021-08-10 16:48:10 +0900

    Initial Commit

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-var question = document.getElementById("question");
-var wrong = document.getElementById("wrong");
+const question = document.getElementById("question");
+const wrong = document.getElementById("wrong");
 
 // 問題
 const words = [
@@ -13,46 +13,42 @@ const ans =[
     "Following",
     "available"
 ];
+
 // 初期化処理
 function init(){
-    // 一番から順番に出す
-    // 問題の表示
+    alert("good")
     form.input[numb].value = ''
     form.input[numb].focus()
-    
 }
+
 // 問題をランダムにださず今回はパス
 // const rnd = Math.floor(Math.random() * words.length);
 
 // 問題の順番をセット
-let numb = 0;
+const numb = 0;
 // 正解の合計数を計算
-let count = 0;
+const count = 0;
 
 // 送信ボタンを取得
 const btn = document.getElementById("submit");
-// 送信ボタンがクリックされたらイベント発生
-// alert(numb <= words.length);
+
 // while(numb <= words.length){
     
-
+    
     // 問題の表示
     question.textContent = words[numb];
-    btn.addEventListener('click', function()
-    {
-        // 解答の取得
-        const form = document.getElementById("ans");
-        // alert(numb);
-        if (form.value === ans[num]){
-            numb = numb + 1
+    
+    //     // 解答の取得
+    const form = document.getElementById("ans");
+    
+    // 送信ボタンがクリックされたらイベント発生
+    function buttonClick(){
+        if (form.value === ans[numb]){
             count++
             init();
         }else{
             wrong.textContent = "間違いだよ！ぼけ！かす！こらっ！ちゃんと勉強せんかい！";
-            num++
-            setTimeout(function(){init()}, 10000)
+            setTimeout(function(){init()}, 1000)
         }
-        
-    }, false);
-    
+    }
 // }
